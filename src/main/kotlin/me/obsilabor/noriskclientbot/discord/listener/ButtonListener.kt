432197344e27c1dbd573a31792907a958d420b9d
mooziii <63669478+mooziii@users.noreply.github.com>
2021-08-25 23:03:57 +0200

    change interaction type

--- src/main/kotlin/me/obsilabor/noriskclientbot/discord/listener/ButtonListener.kt
+++ src/main/kotlin/me/obsilabor/noriskclientbot/discord/listener/ButtonListener.kt
@@ -11,6 +11,7 @@ import dev.kord.core.behavior.createTextChannel
 import dev.kord.core.behavior.interaction.followUpEphemeral
 import dev.kord.core.entity.channel.TextChannel
 import dev.kord.core.entity.interaction.ComponentInteraction
+import dev.kord.core.event.interaction.ButtonInteractionCreateEvent
 import dev.kord.core.event.interaction.InteractionCreateEvent
 import dev.kord.core.on
 import dev.kord.rest.builder.message.create.actionRow
@@ -28,60 +29,58 @@ import org.litote.kmongo.findOne
 class ButtonListener : Listener {
 
     override fun register(client: Kord) {
-        client.on<InteractionCreateEvent> {
+        client.on<ButtonInteractionCreateEvent> {
             val interaction = this.interaction
-            if(interaction is ComponentInteraction) {
-                for(ticket in MongoDatabase.tickets.find()) {
-                    if((interaction).componentId == ticket.closeButtonId) {
-                        val tckit = MongoDatabase.tickets.findOne { Ticket::closeButtonId eq (interaction).componentId }
-                        val channel = tckit?.channelId?.let { nrcGuild.getChannel(it) }
-                        MongoDatabase.tickets.deleteOne(Ticket::closeButtonId eq (interaction).componentId)
-                        channel?.delete("Closed by ${interaction.member().username}")
+            for(ticket in MongoDatabase.tickets.find()) {
+                if((interaction).componentId == ticket.closeButtonId) {
+                    val tckit = MongoDatabase.tickets.findOne { Ticket::closeButtonId eq (interaction).componentId }
+                    val channel = tckit?.channelId?.let { nrcGuild.getChannel(it) }
+                    MongoDatabase.tickets.deleteOne(Ticket::closeButtonId eq (interaction).componentId)
+                    channel?.delete("Closed by ${interaction.member().username}")
+                }
+            }
+            if((interaction).componentId == "TICKET") {
+                var created = false
+                for (ticket in MongoDatabase.tickets.find()) {
+                    if(ticket.member == this.interaction.member().id) {
+                        created = true
                     }
                 }
-                if((interaction).componentId == "TICKET") {
-                    var created = false
-                    for (ticket in MongoDatabase.tickets.find()) {
-                        if(ticket.member == this.interaction.member().id) {
-                            created = true
-                        }
+                if(!created) {
+                    val id = MongoDatabase.tickets.find().toList().size+1
+                    val channel = nrcGuild.createTextChannel("Ticket #$id") {
+                        parentId = Snowflake(ConfigManager.noRiskClientBotConfig.supportCategoryId ?: "supportCategoryId is null!")
                     }
-                    if(!created) {
-                        val id = MongoDatabase.tickets.find().toList().size+1
-                        val channel = nrcGuild.createTextChannel("Ticket #$id") {
-                            parentId = Snowflake(ConfigManager.noRiskClientBotConfig.supportCategoryId ?: "supportCategoryId is null!")
-                        }
-                        channel.editMemberPermission(interaction.member().id) {
-                            allowed.plus(Permission.ReadMessageHistory)
-                            allowed.plus(Permission.SendMessages)
-                            allowed.plus(Permission.ViewChannel)
-                        }
-                        channel.createMessage {
-                            content = "A staff member will help you soon, please describe your problem as detailed as possible ${interaction.member().mention}!"
-                            actionRow {
-                                interactionButton(
-                                    style = ButtonStyle.Danger,
-                                    customId = "close_$id"
-                                ) {
-                                    label = "Close ticket"
-                                    emoji = DiscordPartialEmoji(name = Emojis.noEntry.unicode)
-                                }
+                    channel.editMemberPermission(interaction.member().id) {
+                        allowed.plus(Permission.ReadMessageHistory)
+                        allowed.plus(Permission.SendMessages)
+                        allowed.plus(Permission.ViewChannel)
+                    }
+                    channel.createMessage {
+                        content = "A staff member will help you soon, please describe your problem as detailed as possible ${interaction.member().mention}!"
+                        actionRow {
+                            interactionButton(
+                                style = ButtonStyle.Danger,
+                                customId = "close_$id"
+                            ) {
+                                label = "Close ticket"
+                                emoji = DiscordPartialEmoji(name = Emojis.noEntry.unicode)
                             }
                         }
-                        MongoDatabase.mongoScope.launch {
-                            MongoDatabase.tickets.insertOne(Ticket(
-                                channel.id,
-                                "close_$id",
-                                interaction.member().id
-                            ))
-                        }
-                        interaction.acknowledgeEphemeral().followUpEphemeral {
-                            content = "Ticket created"
-                        }
-                    } else {
-                        interaction.acknowledgeEphemeral().followUpEphemeral {
-                            content = "You already have an open ticket!"
-                        }
+                    }
+                    MongoDatabase.mongoScope.launch {
+                        MongoDatabase.tickets.insertOne(Ticket(
+                            channel.id,
+                            "close_$id",
+                            interaction.member().id
+                        ))
+                    }
+                    interaction.acknowledgeEphemeral().followUpEphemeral {
+                        content = "Ticket created"
+                    }
+                } else {
+                    interaction.acknowledgeEphemeral().followUpEphemeral {
+                        content = "You already have an open ticket!"
                     }
                 }
             }
