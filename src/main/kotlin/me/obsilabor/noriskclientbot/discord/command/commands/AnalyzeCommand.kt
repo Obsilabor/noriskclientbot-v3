0335c552c8df55ffe7f2df91e4b4ba13b3ea4918
mooziii <63669478+mooziii@users.noreply.github.com>
2021-08-26 14:17:46 +0200

    wtf

--- src/main/kotlin/me/obsilabor/noriskclientbot/discord/command/commands/AnalyzeCommand.kt
+++ src/main/kotlin/me/obsilabor/noriskclientbot/discord/command/commands/AnalyzeCommand.kt
@@ -13,8 +13,10 @@ import io.ktor.client.engine.cio.*
 import io.ktor.client.features.json.*
 import io.ktor.client.request.*
 import io.ktor.http.*
+import kotlinx.coroutines.launch
 import me.obsilabor.noriskclientbot.config.ConfigManager
 import me.obsilabor.noriskclientbot.data.*
+import me.obsilabor.noriskclientbot.database.MongoDatabase
 import me.obsilabor.noriskclientbot.discord.command.AdvancedCommand
 import me.obsilabor.noriskclientbot.discord.command.CommandCategory
 import me.obsilabor.noriskclientbot.extensions.*
@@ -41,17 +43,19 @@ object AnalyzeCommand : AdvancedCommand(
     override suspend fun handle(interaction: ChatInputCommandInteraction) {
         if(interaction.member().hasPermission(Permission.Administrator)) {
             val comment = interaction.command.options["comment"]?.value.toString().replace("\"", "'")
-            val response = httpClient.request<ToxicityResult>("https://commentanalyzer.googleapis.com/v1alpha1/comments:analyze?key=${ConfigManager.noRiskClientBotConfig.perspectiveApiKey ?: error("perspectiveApiKey is null!")}") {
-                contentType(ContentType.Application.Json)
-                body = Request(
-                    RequestComponent(
-                        TextComponent(comment),
-                        arrayListOf("de"),
-                        AttributeScores(Toxicity(null, null))
+            MongoDatabase.mongoScope.launch {
+                val response = httpClient.request<ToxicityResult>("https://commentanalyzer.googleapis.com/v1alpha1/comments:analyze?key=${ConfigManager.noRiskClientBotConfig.perspectiveApiKey ?: error("perspectiveApiKey is null!")}") {
+                    contentType(ContentType.Application.Json)
+                    body = Request(
+                        RequestComponent(
+                            TextComponent(comment),
+                            arrayListOf("de"),
+                            AttributeScores(Toxicity(null, null))
+                        )
                     )
-                )
+                }
+                println(response.attributeScores.TOXICITY.summaryScore?.value)
             }
-            println(response.attributeScores.TOXICITY.summaryScore?.value)
             interaction.acknowledgePublic().followUp {
                 embed {
                     title = "Toxicity Report"
@@ -63,7 +67,7 @@ object AnalyzeCommand : AdvancedCommand(
                     thumbnail {
                         url = interaction.guild().getIconUrl(Image.Format.GIF)!!
                     }
-                    description = "This comment has a toxicity value of `${response.attributeScores.TOXICITY.summaryScore?.value?.cut()}%`"
+                    description = "Look in console"
                 }
             }
         } else {
